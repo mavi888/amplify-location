49d4434ea85eb331079c7433992b89564f26ecbd
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2021-01-29 12:49:31 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,150 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
+import ReactMapGL, {
+  NavigationControl,
+} from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import Amplify, { Auth } from 'aws-amplify';
+import { Signer } from "@aws-amplify/core";
+import Location from "aws-sdk/clients/location";
+import awsconfig from './aws-exports';
+
+const mapName = "MyMapTest";
+
+Amplify.configure(awsconfig);
+
+/**
+ * Sign requests made by Mapbox GL using AWS SigV4.
+ */
+const transformRequest = (credentials) => (url, resourceType) => {
+  // Resolve to an AWS URL
+  if (resourceType === "Style" && !url?.includes("://")) {
+    url = `https://maps.geo.${awsconfig.aws_project_region}.amazonaws.com/maps/v0/maps/${url}/style-descriptor`;
+  }
+
+  // Only sign AWS requests (with the signature as part of the query string)
+  if (url?.includes("amazonaws.com")) {
+    return {
+      url: Signer.signUrl(url, {
+        access_key: credentials.accessKeyId,
+        secret_key: credentials.secretAccessKey,
+        session_token: credentials.sessionToken,
+      })
+    };
+  }
+
+  // Don't sign
+  return { url: url || "" };
+};
+
+function Search(props){
+
+  const [place, setPlace] = useState('Helsinki');
+ 
+  const handleChange = (event) => {
+    setPlace(event.target.value);
+  }
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    props.searchPlace(place)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <div className="input-group">
+        <input type="text" className="form-control form-control-lg" placeholder="Search for Places" aria-label="Place" aria-describedby="basic-addon2" value={ place } onChange={handleChange}/>
+        <div className="input-group-append">
+          <button onClick={ handleClick } className="btn btn-primary" type="submit">Search</button>
+        </div>
+      </div>
+    </div>
+  )
+};
+
+const App = () => {
+  const [credentials, setCredentials] = useState(null);
+
+  const [viewport, setViewport] = useState({
+    longitude: -123.1187,
+    latitude: 49.2819,
+    zoom: 10,
+  });
+
+  const [client, setClient] = useState(null);
+ 
+
+  useEffect(() => {
+    const fetchCredentials = async () => {
+      setCredentials(await Auth.currentUserCredentials());
+    };
+
+    fetchCredentials();
+
+    const createClient = async () => {
+      const credentials = await Auth.currentCredentials();
+      const client = new Location({
+          credentials,
+          region: awsconfig.aws_project_region,
+     });
+     setClient(client);
+    }
+
+    createClient();
+  }, []);
+
+  const searchPlace = (place) => {
+
+    const params = {
+      IndexName: "MyPlaceIndex",
+      Text: place,
+    };
+
+    client.searchPlaceIndexForText(params, (err, data) => {
+      if (err) console.error(err);
+      if (data) {
+ 
+        const coordinates = data.Results[0].Place.Geometry.Point;
+        setViewport({
+          longitude: coordinates[0],
+          latitude: coordinates[1], 
+          zoom: 10})
+        return coordinates;
+      }
+    });
+  }
+
+
+  return (
+    <div>
+      <header>
+        <h1>FooBar Maps</h1>
       </header>
+       <div>
+        <Search searchPlace = {searchPlace} /> 
+      </div>
+      {credentials ? (
+        <ReactMapGL
+          {...viewport}
+          width="100%"
+          height="100vh"
+          transformRequest={transformRequest(credentials)}
+          mapStyle={mapName}
+          onViewportChange={setViewport}
+        >
+          <div style={{ position: "absolute", left: 20, top: 20 }}>
+            {/* react-map-gl v5 doesn't support dragging the compass to change bearing */}
+            <NavigationControl showCompass={false} />
+          </div>
+        </ReactMapGL>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
   );
-}
+};
 
 export default App;
